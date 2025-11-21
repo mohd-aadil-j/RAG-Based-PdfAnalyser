93aedf3fe1ece27ab39b857d58d9ab9a9e5b9bbf
kavya121224 <mohamadaadilj@gmail.com>
2025-11-21 12:45:16 +0530

    update in Imports

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import dotenv
 import streamlit as st
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from langchain_groq import ChatGroq
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 
 from langchain.chains import ConversationalRetrievalChain
